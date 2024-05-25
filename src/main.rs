12102b9e87153f50a5789844ec9e04f54f025eb1
jiisanda <jiisanda@protonmail.com>
2024-05-25 21:25:07 +0530

    ifname change

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use tun_tap::Mode;
 
 fn main() -> io::Result<()> {
-    let i_face = tun_tap::Iface::new("my_tun", Mode::Tun)?;
+    let i_face = tun_tap::Iface::new("tun0", Mode::Tun)?;
     let mut buffer = [0u8; 1504];
     let nbytes = i_face.recv(&mut buffer[..])?;
     eprintln!("read {} bytes: {:x?}", nbytes, &buffer[..nbytes]);
