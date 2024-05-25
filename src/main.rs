945ccd4265e9d1dbda3be9fdcf0889992de414ba
jiisanda <harshjaiswal2307@gmail.com>
2024-05-25 17:40:14 +0530

    first tun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use tun_tap::Mode;
+
+fn main() -> io::Result<()> {
+    let i_face = tun_tap::Iface::new("my_tun", Mode::Tun)?;
+    let mut buffer = [0u8; 1504];
+    let nbytes = i_face.recv(&mut buffer[..])?;
+    eprintln!("read {} bytes: {:x?}", nbytes, &buffer[..nbytes]);
+    Ok(())
 }
