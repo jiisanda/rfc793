bdd647dc6b18c1490d6ba9404596d97c878ce69d
jiisanda <jiisanda@protonmail.com>
2024-05-27 20:22:00 +0530

    parsing ipv4

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() -> io::Result<()> {
     let i_face = tun_tap::Iface::new("tun0", Mode::Tun)?;
     let mut buffer = [0u8; 1504];
     loop {
-        let nbytes = i_face.recv(&mut buffer[..])?;
+        let n_bytes = i_face.recv(&mut buffer[..])?;
 
         // TUN Frame Format: flags: 2 bytes; proto: 2 bytes;
         let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
@@ -14,13 +14,21 @@ fn main() -> io::Result<()> {
         if proto != 0x0800 {
             continue;           // no ipv4
         }
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buffer[4..nbytes]
-        );
+
+        match etherparse::Ipv4Slice::from_slice(&buffer[4..n_bytes]) {
+            Ok(p) => {
+                eprintln!(
+                    "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
+                    n_bytes - 4,
+                    flags,
+                    proto,
+                    p
+                );
+            },
+            Err(e) => {
+                eprintln!("ignoring wired packet {:?}", e)
+            }
+        }
     }
     // Ok(())
 }
