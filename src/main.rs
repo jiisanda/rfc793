986add331e7493cfed645f76952f79aff39c3693
jiisanda <jiisanda@protonmail.com>
2024-05-28 11:29:44 +0530

    parsing packet - etherparse

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,23 @@ fn main() -> io::Result<()> {
         let n_bytes = i_face.recv(&mut buffer[..])?;
 
         // TUN Frame Format: flags: 2 bytes; proto: 2 bytes;
-        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
-        let proto = u16::from_be_bytes([buffer[2], buffer[3]]);
-        if proto != 0x0800 {
+        let _eth_flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let eth_proto = u16::from_be_bytes([buffer[2], buffer[3]]);
+        if eth_proto != 0x0800 {
             continue;           // no ipv4
         }
 
-        match etherparse::Ipv4Slice::from_slice(&buffer[4..n_bytes]) {
+        match etherparse::Ipv4HeaderSlice::from_slice(&buffer[4..n_bytes]) {
             Ok(p) => {
+                let src = p.source_addr();
+                let dest = p.destination_addr();
+                let proto = p.protocol();
                 eprintln!(
-                    "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-                    n_bytes - 4,
-                    flags,
-                    proto,
-                    p
+                    "{} → {} {:?}b of protocol {:}",
+                    src,
+                    dest,
+                    p.payload_len(),
+                    proto
                 );
             },
             Err(e) => {
